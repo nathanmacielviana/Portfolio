001222c6c63385e4546ef093a18ec1b51ec42f82
Nathan Maciel <112430114+nathanmacielviana@users.noreply.github.com>
2023-06-26 11:03:18 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-let header = document.querySelector('header');
+let menuIcon = document.querySelector('#openmenu');
+let navbar = document.querySelector('.navbar');
 
-header.classList.toggle('sticky', window.scrollY > 100);
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('#closemenu');
+}
+
+/*remover botão*/
+menuIcon = document.remove('#openmenu');
+navbar = document.remove('.navbar');
+
+/*scroll reveal*/
+ScrollReveal({
+	 reset: true,
+	 distance:'80px',
+	 duration: 2000,
+	 delay: 200 
+	});
+
+ScrollReveal().reveal('.homecontent, .servicos, .projetos, contato', { origin: 'top' });
