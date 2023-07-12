d357fb38297f51537869f6193380d296cc6bb86f
Nathan Maciel <112430114+nathanmacielviana@users.noreply.github.com>
2023-07-12 10:28:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
-openmenu.addEventListener('click', () => {
-	menu.style.display = "flex"
-	menu.style.right = (menu.offsetWidth * -1) + 'px'
-
-	setTimeout(()=> {
-		menu.style.opacity = '1'
-		menu.style.right = "0"
-		openmenu.style.display = 'none'
-	}, 10);
-})
-
-closemenu.addEventListener('click', () => {
-	menu.style.opacity = '0'
-	menu.style.right = (menu.offsetWidth * -1) + 'px'
-	setTimeout(()=> {
-		menu.removeAttribute('style')
-		openmenu.removeAttribute('style')
-	}, 200);
-})
+class MobileNavbar {
+	constructor(mobileMenu, navList, navLinks) {
+	  this.mobileMenu = document.querySelector(mobileMenu);
+	  this.navList = document.querySelector(navList);
+	  this.navLinks = document.querySelectorAll(navLinks);
+	  this.activeClass = "active";
+  
+	  this.handleClick = this.handleClick.bind(this);
+	}
+  
+	animateLinks() {
+	  this.navLinks.forEach((link, index) => {
+		link.style.animation
+		  ? (link.style.animation = "")
+		  : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+			  index / 7 + 0.3
+			}s`);
+	  });
+	}
+  
+	handleClick() {
+	  this.navList.classList.toggle(this.activeClass);
+	  this.mobileMenu.classList.toggle(this.activeClass);
+	  this.animateLinks();
+	}
+  
+	addClickEvent() {
+	  this.mobileMenu.addEventListener("click", this.handleClick);
+	}
+  
+	init() {
+	  if (this.mobileMenu) {
+		this.addClickEvent();
+	  }
+	  return this;
+	}
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+	".mobilemenu",
+	".navlist",
+	".navlist li",
+  );
+  mobileNavbar.init();
